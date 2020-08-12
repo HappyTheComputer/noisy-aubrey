7715d2ff526a14dcdbb7b673676cce4e9ffe160b
lilyFortuneTime <lily.chen@maxfortune.tw>
2020-08-12 11:04:12 +0800

    獨立回覆訊息函式於replyFriend

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ import replyFriend as rf
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
-    rf.reply_spideypool_party(line_bot_api, event)
+    rf.reply_method(line_bot_api, event)
 
 @app.route('/static/<path:path>')
 def send_static_content(path):
