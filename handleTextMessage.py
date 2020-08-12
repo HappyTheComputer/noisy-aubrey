6f53eb7428bf00fa235d8b63bf3ca3ed27a82349
lilyFortuneTime <lily.chen@maxfortune.tw>
2020-08-12 16:13:48 +0800

    fix

--- handleTextMessage.py
+++ handleTextMessage.py
@@ -95,7 +95,7 @@ def check_text_key(text, event):
             "11538":[51626494, 51626533],
             "11539":[52114110, 52114149]
         }
-        packageIDs = askGod.keys()
+        packageIDs = askGod.keys().copy()
         package = random.choice(packageIDs)
         sticker = random.randint(askGod[package][0], askGod[package][1])
         line_bot_api.reply_message(
