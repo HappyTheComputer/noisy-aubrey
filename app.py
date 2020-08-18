d74c7da289195818d6ed8c643d47dc25a19fea0f
lilyFortuneTime <lily.chen@maxfortune.tw>
2020-08-18 15:01:36 +0800

    change pick text

--- app.py
+++ app.py
@@ -53,11 +53,13 @@ app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_proto=1)
 
 # get channel_secret and channel_access_token from your environment variable
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variables.')
+if channel_secret is None or channel_access_token is None:
+    print('Specify LINE_CHANNEL_SECRET or LINE_CHANNEL_ACCESS_TOKEN as environment variables.')
     sys.exit(1)
 
+line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
