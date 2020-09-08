fbec022912e298605b1fe59d40055b310335d944
HappyTheComputer <happythecomputer@gmail.com>
2020-09-08 16:12:02 +0800

    change greet key on thursday

--- ScheduleBot.py
+++ ScheduleBot.py
@@ -10,9 +10,9 @@ def greet_worker():
         '各位社畜加油吧，週一來一寶～', 
         '週二難熬啊，甜心隊長給你加油！', 
         '這週過半啦，週三啊各位！', 
-        '今天是雷神之日。', 
+        '所以，有鷹眼的早餐嗎？', 
         '要放假啦！嗨起來！']
-    greetWeekImg = ['Thomas+Holland+spiderman', 'Chris+Evans+cap', 'Ryan+Reynolds', 'Thor+Odinson+Chris', 'Robert+Downey', 'Loki+Tom＋Hiddleston']
+    greetWeekImg = ['Thomas+Holland+spiderman', 'Chris+Evans+cap', 'Ryan+Reynolds', 'Clint+Barton+Hawkeye', 'Robert+Downey', 'Loki+Tom＋Hiddleston']
     today=int(time.strftime("%w"))-1
     if today >= 0 and today <= 5:
         todayImages = search_image(greetWeekImg[today], 100)
